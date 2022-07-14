961053cfd55b02e03b930d1b0510def32bab2c2d
Anastasia <vorobyova.86@mail.ru>
2022-07-14 23:07:53 +0300

    task6 well done,finally

--- Lesson1_task6/Program.cs
+++ Lesson1_task6/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿int numberA = 60;
+int result = numberA%2;
+if (result == 0)
+{
+Console.WriteLine (numberA + " Четное число? да");
+}
+else {
+Console.WriteLine (numberA + " Четное число? нет");
+}
