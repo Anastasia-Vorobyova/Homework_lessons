9ba6ed180e984ee8719a3630cae1c329023518f9
Anastasia <vorobyova.86@mail.ru>
2022-07-14 22:17:04 +0300

    task2 well done

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2 к семинару 1 C#
+
+int numberA = 5;
+int numberB = 7;
+
+if (numberA>numberB)
+{
+    Console.WriteLine("максимальное число "+numberA);
+    Console.WriteLine("минимальное число "+numberB);
+}
+else {
+    Console.WriteLine("максимальное число "+numberB);
+    Console.WriteLine("минимальное число "+numberA);
+}
