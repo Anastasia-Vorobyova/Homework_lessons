f9b3f8a6c4558f70f7b84b4b3fa7c8fe1ed9c1fc
Anastasia <vorobyova.86@mail.ru>
2022-07-14 22:39:21 +0300

    task4 well done

--- Lesson1_task4/Program.cs
+++ Lesson1_task4/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿ int numberA = 99;
+ int numberB = 10;
+ int numberC = 80;
+ int max= numberA;
+
+ if(numberA>max) max=numberA;
+  if(numberB>max) max=numberB;
+   if(numberC>max) max=numberC;
+
+   Console.WriteLine ("Максимальное число "+max);
