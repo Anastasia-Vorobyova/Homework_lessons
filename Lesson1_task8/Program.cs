53a241a99cfe06a2be07cf1c8feddadd92143187
Anastasia <vorobyova.86@mail.ru>
2022-07-14 23:49:48 +0300

    task6 is well done

--- Lesson1_task8/Program.cs
+++ Lesson1_task8/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 15;
+
+for(int i=1; i<=numberA; i++)
+{
+    if(i%2==0)
+    {
+        Console.WriteLine(i);
+    }
+    
+}
