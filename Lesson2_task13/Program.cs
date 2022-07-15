101e7b420084f2073965af347cea1de990b09a72
Anastasia <vorobyova.86@mail.ru>
2022-07-15 22:59:33 +0300

    task13 is well done

--- Lesson2_task13/Program.cs
+++ Lesson2_task13/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программа, которая выводит 3ю цифру числа, или сообщает, что 3й цифры нет
+Console.WriteLine ("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+string some_string;
+if (number > 0)
+{
+   Console.WriteLine("Ввели число "+ number);
+   if (number>100) 
+   {
+    some_string = Convert.ToString(number);
+    Console.WriteLine ("Третье число ");
+    Console.WriteLine(some_string[2]); 
+   } 
+   else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+}
+else
+{
+    Console.WriteLine("Число меньше 1");
+}
