0897d9ff5b3aa47d9bf814355b978ddb3f678494
Anastasia <vorobyova.86@mail.ru>
2022-07-15 22:40:30 +0300

    task10 is well done

--- Lesson2_task10/Program.cs
+++ Lesson2_task10/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// программа, которая принимает на вход 3х значное число и на выходе показывает 2ю цифру этого числа
+Console.WriteLine ("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+string some_string;
+if (number >=100 && number<1000)
+{
+    some_string = Convert.ToString(number);
+    Console.WriteLine(some_string[1]); 
+}
+else
+{
+    Console.WriteLine("Error");
+}
