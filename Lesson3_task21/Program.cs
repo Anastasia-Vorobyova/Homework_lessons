81cdfd3b0896a5fcb6cd2b1ef81a3d530e64165e
Anastasia <vorobyova.86@mail.ru>
2022-07-17 22:18:50 +0300

    task21 is done

--- Lesson3_task21/Program.cs
+++ Lesson3_task21/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+void PrintShape(double ax, double ay, double az, double bx, double by, double bz)
+{
+    double deltaX = ax - bx; 
+    double deltaY = ay - by; 
+    double deltaZ = az - bz; 
+    double c = Math.Sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+    Console.WriteLine(c);
+}
+PrintShape(7,-5, -1, 1,-1,16);
