5f2ac531d0ae34726f151c729845e1a740dd4090
Anastasia <vorobyova.86@mail.ru>
2022-07-19 22:45:14 +0300

    task25 is done

--- Lesson4_task25/Program.cs
+++ Lesson4_task25/Program.cs
@@ -1,4 +1,15 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
+Console.WriteLine("Введите число A");
+int A= int.Parse(Console.ReadLine());
 
+Console.WriteLine("Введите число B");
+int B= int.Parse(Console.ReadLine());
 
+int sum = 1;
+
+for (int i=0; i<B; i++)
+{
+    sum=sum*A;
+}
+    System.Console.WriteLine(sum);
