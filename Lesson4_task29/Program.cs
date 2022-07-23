9a12930068a37dc38382d0ca6ecd732d4304351a
Anastasia <vorobyova.86@mail.ru>
2022-07-23 19:08:59 +0300

    task29 is done

--- Lesson4_task29/Program.cs
+++ Lesson4_task29/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] array = new int [8];
+int length = array.Length;
+
+for (int i = 0; i < length; i++)
+{
+    array[i] = new Random().Next(0, 8);
+}
+
+foreach (var item in array)
+{
+  Console.WriteLine(item + " ");  
+}
 
