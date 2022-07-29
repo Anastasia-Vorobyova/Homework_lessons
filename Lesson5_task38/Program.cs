3539bf690d1394c01542ba14f99c8eeacc83f0fa
Anastasia <vorobyova.86@mail.ru>
2022-07-29 15:02:12 +0300

    task38 is done

--- Lesson5_task38/Program.cs
+++ Lesson5_task38/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int [] array = new int [20];
+float min;
+float max;
+float diff;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,90);
+}
+
+max = array.Max();
+min = array.Min();
+diff = max-min;
+
+Console.WriteLine(String.Join(", ", array));
+
+System.Console.WriteLine("Разница между мин макс "+diff);
+
+
