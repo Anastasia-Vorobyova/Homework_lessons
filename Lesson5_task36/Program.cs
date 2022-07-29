710c62ea43c30f7b7eeaa1722072e66fe306a858
Anastasia <vorobyova.86@mail.ru>
2022-07-29 13:20:44 +0300

    task36 is done

--- Lesson5_task36/Program.cs
+++ Lesson5_task36/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int [] array = new int [10];
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,10);
+}
+
+System.Console.WriteLine(String.Join(", ", array));
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i%2 == 0) 
+    {
+    sum = sum + array [i];
+    }     
+}
+Console.WriteLine("Вывод суммы " + sum);
