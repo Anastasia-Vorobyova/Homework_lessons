f25154f964d9b32d4f676280330977c12d33a99b
Anastasia <vorobyova.86@mail.ru>
2022-08-06 17:35:54 +0300

    task54 is done

--- Lesson8_task54/Program.cs
+++ Lesson8_task54/Program.cs
@@ -1,2 +1,55 @@
 ﻿// Задача 54: Задайте двумерный массив. 
 //Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SwapElementsInMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1)-1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] array = GetMatrix(4, 4, 1, 100);
+PrintMatrix(array);
+System.Console.WriteLine();
+SwapElementsInMatrix(array);
+System.Console.WriteLine();
+PrintMatrix(array);
