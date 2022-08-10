91bcd8cc452ccdfaed170dff4562d96f9ab87b13
Anastasia <vorobyova.86@mail.ru>
2022-08-10 18:05:13 +0300

    task64 is done

--- Lesson9_task64/Program.cs
+++ Lesson9_task64/Program.cs
@@ -1,1 +1,16 @@
 ﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+void FindSum(int m, int n, int sum = 0)
+{
+
+    if (m > n)
+    {
+        System.Console.WriteLine("сумма элементов " + sum);
+        return;
+    }
+
+    FindSum(m + 1, n, sum += m);
+    return;
+}
+
+FindSum(4, 8);
